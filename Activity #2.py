a516e975721c55bf04d4719ca00fc2a86b14c268
jaypbazar <jaypbazar@cspceduc.onmicrosoft.com>
2024-09-23 14:50:15 +0800

    test run

--- Activity #2.py
+++ Activity #2.py
@@ -1,12 +1,16 @@
+import os 
+
 class Product:
-    def __init__(self, name):
-        self.name = name
 
     def __init__(self, name, price):
         self.name = name
         self.price = price
 
+    def __str__(self):
+        return f"{self.name} - \u20b1{self.price}"
+
 class ShoppingCart:
+    
     def __init__(self, balance):
         self.user_balance = balance
         self.products = []
@@ -22,7 +26,20 @@ class ShoppingCart:
         pass
 
 def open_menu():
-    pass
+    os.system('cls')
+    print("Welcome to PyShop!")
+    
+    balance = 1000000
+
+    cart = ShoppingCart(balance)
+
+    prod1 = Product("Keyboard", 500)
+    prod2 = Product("Mouse", 200)
+
+    cart.add_product(prod1)
+    cart.add_product(prod2)
+
+    cart.list_products()
 
 if __name__ == "__main__":
     open_menu()
